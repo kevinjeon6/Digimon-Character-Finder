57de2d5eef6121e3d620b3e0987555a4e6bf8d7a
Kevin <kevin_jeon@icloud.com>
2023-04-15 16:31:30 -0400

    Create view model to retreive data

---
+++ DigimonProject/ DigimonViewModel.swift
@@ -0,0 +1,17 @@
+//
+//   DigimonViewModel.swift
+//  DigimonProject
+//
+//  Created by Kevin Mattocks on 4/15/23.
+//
+
+import Foundation
+
+class DigimonViewModel {
+    
+    
+    func getDigimonData() {
+        
+    }
+    
+}
