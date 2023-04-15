a1c240f1e604690e6973005c3f7498f22b278451
Kevin <kevin_jeon@icloud.com>
2023-04-15 17:02:57 -0400

    Declare delegate in view model Add communication between the view model and ViewController using the protocol and delegate pattern

--- DigimonProject/ DigimonViewModel.swift
+++ DigimonProject/ DigimonViewModel.swift
@@ -7,11 +7,21 @@
 
 import Foundation
 
+//To create a delegate you need to create a protocol
+protocol DigimonViewModelProtocol {
+    func didFinish()
+    func didFail(error: Error)
+}
+
+
 class DigimonViewModel {
     
+    //Declare delegate property in view model
+    var delegate: DigimonViewModelProtocol?
     
     func getDigimonData() {
         
+        self.delegate?.didFinish()
     }
     
 }
