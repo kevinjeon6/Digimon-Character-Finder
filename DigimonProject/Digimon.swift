295fa3499a5c4e54fb66f1716aa7849502f57294
Kevin <kevin_jeon@icloud.com>
2023-04-15 16:28:42 -0400

    Make model for JSON for Digimon

---
+++ DigimonProject/Digimon.swift
@@ -0,0 +1,14 @@
+//
+//  Digimon.swift
+//  DigimonProject
+//
+//  Created by Kevin Mattocks on 4/15/23.
+//
+
+import Foundation
+
+struct Digimon: Codable {
+    var name: String
+    var img: String
+    var level: String
+}
