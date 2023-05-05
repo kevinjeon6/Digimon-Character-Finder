f33e52ed037b40cf48934e25cb6e7ff3c3b4dbcf
Kevin <kevin_jeon@icloud.com>
2023-05-05 16:05:39 -0400

    Create a PersistenceManager for persisting the data Create method to retrieve the favorites

---
+++ DigimonProject/PersistenceManager.swift
@@ -0,0 +1,46 @@
+//
+//  PersistanceManager.swift
+//  DigimonProject
+//
+//  Created by Kevin Mattocks on 5/3/23.
+//
+
+import Foundation
+
+
+
+
+enum Keys {
+    static let favorites = "favorites"
+}
+
+//Using an enum. Cannot initialize an empty enum. Small safety to prevent errors since you can initialize an empty struct
+enum PersistenceManager {
+    static private let defaults = UserDefaults.standard
+
+    // MARK: - Retreive Favorites
+    //Retreive the user defaults.
+    //Result in completion handler = A value that represents either a success or a failure, including an associated value in each case.
+    //If successful then we'll get the array of Favorite Digimon. If fail then an error
+    //The as Data is casting as Data because it doesn't the object
+    static func retrieveFavorites(completed: @escaping (Result<[Digimon], Error>) -> Void) {
+        //When saving to defaults. Need to give it a key name
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else { return
+            //First time use when there are no favorites added. We don't want an error to display. Just an empty array
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Digimon].self, from: favoritesData)
+            completed(.success(favorites))
+        
+        } catch {
+            fatalError("Unable to favorite")
+        }
+    }
+    
+   
+    
+}
