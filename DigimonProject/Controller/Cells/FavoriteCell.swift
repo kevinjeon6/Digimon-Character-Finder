1db5df8f27f309dcb375cc26bfd01c2f179811b9
Kevin <kevin_jeon@icloud.com>
2023-05-08 22:54:47 -0400

    Add logic to FavoritesListVC Add static identifier for FavoriteCell and set method

--- DigimonProject/Controller/Cells/FavoriteCell.swift
+++ DigimonProject/Controller/Cells/FavoriteCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class FavoriteCell: UITableViewCell {
     
-
+    static let identifier = "FavoriteCell"
     
     
     private var digimonImageView: UIImageView = {
@@ -42,40 +42,59 @@ class FavoriteCell: UITableViewCell {
         label.minimumScaleFactor = 0.1
         return label
     }()
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-     setupFavoritesUI()
+        setupFavoritesUI()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // convert the URL string to an actual image object and set it to the UIImageView instance in the cell.
+    func setFave(favorite: Digimon) {
+        digimonTitleLabel.text = favorite.name
+        digimonLevelLabel.text = favorite.level
+        
+        
+        guard let imageUrl = URL(string: favorite.img) else { return }
+        
+        DispatchQueue.global().async {
+            if let imageData = try? Data(contentsOf: imageUrl) {
+                let image = UIImage(data: imageData)
+                
+                DispatchQueue.main.async {
+                    self.digimonImageView.image = image
+                }
+            }
+        }
+    }
+    
+
     
     private func setupFavoritesUI() {
-        contentView.addSubview(digimonImageView)
-        contentView.addSubview(digimonTitleLabel)
-        contentView.addSubview(digimonLevelLabel)
+        self.contentView.addSubview(digimonImageView)
+        self.contentView.addSubview(digimonTitleLabel)
+        self.contentView.addSubview(digimonLevelLabel)
         
         
         NSLayoutConstraint.activate([
-            digimonImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            digimonImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            digimonImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            digimonImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 12),
             digimonImageView.heightAnchor.constraint(equalToConstant: 80),
             digimonImageView.widthAnchor.constraint(equalToConstant: 80),
             
-            digimonTitleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            digimonTitleLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
             digimonTitleLabel.leadingAnchor.constraint(equalTo: digimonImageView.trailingAnchor, constant: 14),
-            digimonTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -14),
+            digimonTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -14),
             digimonTitleLabel.heightAnchor.constraint(equalToConstant: 40),
             
             digimonLevelLabel.leadingAnchor.constraint(equalTo: digimonImageView.trailingAnchor, constant: 14),
-            digimonLevelLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -14),
+            digimonLevelLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -14),
             digimonLevelLabel.topAnchor.constraint(equalTo: digimonTitleLabel.bottomAnchor),
         
         ])
--- DigimonProject/Controller/FavoritesListViewController.swift
+++ DigimonProject/Controller/FavoritesListViewController.swift
@@ -8,14 +8,123 @@
 import UIKit
 
 class FavoritesListViewController: UIViewController {
+    
+    // MARK: - Properties
+    var favorites: [Digimon] = []
+    
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemBackground
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.identifier)
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemBlue
+
+        //In a total different Navigation controller since tab bar has different navigation controllers for each tab. Need to customize the nav bar UI
+        configFavoritesTableView()
+        configNavBar()
+        
     }
     
+    
+    //ViewDidLoad only gets called once. But if the user sees they have no favorites and then adds favorites and goes back to favorites list will get called then. Always refreshing favorites
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    
+    // MARK: - Set up UI
+    func configFavoritesTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds //telling how big the table view should be. View.bounds takes up the whole view
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.removeExcessCells()
+        
+    }
+    
+    func configNavBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .mainOrange()
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.barStyle = .black //bar style gives us the white status bar/white text look
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.title = "Favorites"
+    }
+    
+    
+    
+    
+    func getFavorites() {
+        let results = PersistenceManager.retrieveFavorites()
+        
+        switch results {
+        case .success(let favorites):
+            if favorites.isEmpty {
+            self.showEmptyView(with: "You do not have any favorites yet", in: self.view)
+            } else {
+                self.favorites = favorites
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    self.view.bringSubviewToFront(self.tableView)
+                }
+            }
+        case .failure:
+            break
+        }
+    }
+}
 
 
+// MARK: - Extension for the TableView
+extension FavoritesListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.identifier, for: indexPath) as?
+                FavoriteCell else {
+            fatalError("Could not dequeue favoritecell")
+        }
+        
+        //Get favorites
+        let favorite = favorites[indexPath.row]//IndexPath.row is grabbing the index from the array for that row
+        cell.setFave(favorite: favorite)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return }
+        let deleteFavorite = favorites[indexPath.row]
+        let savingError = PersistenceManager.updateWith(favorite: deleteFavorite, actionType: .remove)
+        if let savingError {
+            //Add an alert here. Unable to remove
+            return
+        } else {
+            favorites.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+            
+            if favorites.isEmpty {
+                showEmptyView(with: "No favorite digimon added yet", in: self.view)
+            }
+        }
+    }
 }
