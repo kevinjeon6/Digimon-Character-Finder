ecf9b905587a8a168ab041813d4e26ea5cac31cd
Kevin <kevin_jeon@icloud.com>
2023-05-09 16:09:10 -0400

    Create alert body text for reusable alert

---
+++ DigimonProject/Utilities/AlertBodyLabel.swift
@@ -0,0 +1,40 @@
+//
+//  AlertBodyLabel.swift
+//  DigimonProject
+//
+//  Created by Kevin Mattocks on 5/9/23.
+//
+
+import UIKit
+
+class AlertBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    
+    // MARK: - Set up UI
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.7
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+}
