1dc140205288103ce3e0d45038e15de4c299521b
Kevin <kevin_jeon@icloud.com>
2023-05-08 20:09:25 -0400

    Add extensions for TableView

--- DigimonProject/Helpers/Extensions.swift
+++ DigimonProject/Helpers/Extensions.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// MARK: - Color extension
 extension UIColor {
     static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
@@ -16,4 +17,19 @@ extension UIColor {
         return UIColor.rgb(red: 232, green: 97, blue: 0)
     }
     
+}
+
+// MARK: - Table View extension
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+    
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
 }
